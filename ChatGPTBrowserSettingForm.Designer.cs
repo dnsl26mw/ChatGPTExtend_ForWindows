ec89f28abbd283acc45d26814939b294c9633bda
dnsl26mw <dnsl26mw@gmail.com>
2026-02-13 00:15:35 +0900

    設定画面用ファイル追加

---
+++ ChatGPTBrowserSettingForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace ChatGPTBrowser
+{
+	partial class ChatGPTBrowserSettingForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatGPTBrowserSettingForm));
+			this.SuspendLayout();
+			// 
+			// ChatGPTBrowserSettingForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 411);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(300, 450);
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(300, 450);
+			this.Name = "ChatGPTBrowserSettingForm";
+			this.Text = "ChatGPTBrowser設定";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
